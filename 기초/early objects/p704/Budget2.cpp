3e9c6cddc707ebcf634fe81e0db511cf92322326
myoungjin.na <myoungjin.na@gmail.com>
2016-04-10 21:10:46 +0900

    폴더 정리 함.

--- 기초/early objects/p704/Budget2.cpp
+++
@@ -1,8 +0,0 @@
-#include "Budget2.h"
-
-double Budget::corpBudget = 0;
-
-void Budget::mainOffice(double budReq)
-{
-	corpBudget += budReq;
-}
--- 기초/early objects/p761/Inheritance.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Inheritance
-{
-public:
-	Inheritance();
-	~Inheritance();
-};
-
